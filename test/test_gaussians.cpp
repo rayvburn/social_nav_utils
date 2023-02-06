9e1ed71492b4a258e9334e969122362b18e5ffb3
Jarek Karwowski <chromedivizer@gmail.com>
2023-02-06 15:02:00 +0100

    added unit tests for Gaussian functions

--- test/test_gaussians.cpp
+++ test/test_gaussians.cpp
@@ -4,6 +4,37 @@
 
 using namespace social_nav_utils;
 
+TEST(TestGaussians, univariate) {
+	// Matlab: normpdf(0, 0, 1)
+	auto g1 = calculateGaussian(0.0, 0.0, std::pow(1.0, 2));
+	ASSERT_NEAR(g1, 0.3989, 1e-03);
+
+	// Matlab: normpdf(0, 0, 2)
+	auto g2 = calculateGaussian(0.0, 0.0, std::pow(2.0, 2));
+	ASSERT_NEAR(g2, 0.1995, 1e-03);
+
+	// always 1.0 at mean
+	auto g3 = calculateGaussian(0.0, 0.0, std::pow(3.0, 2), true);
+	ASSERT_DOUBLE_EQ(g3, 1.0);
+}
+
+TEST(TestGaussians, univariateAngleDomain) {
+	auto g1 = calculateGaussianAngle(M_PI, -M_PI, std::pow(1.0, 2));
+	ASSERT_NEAR(g1, 0.3989, 1e-03);
+
+	auto g2 = calculateGaussianAngle(-M_PI, M_PI, std::pow(2.0, 2));
+	ASSERT_NEAR(g2, 0.1995, 1e-03);
+
+	auto g3 = calculateGaussianAngle(-M_PI, M_PI, std::pow(3.0, 2), true);
+	ASSERT_DOUBLE_EQ(g3, 1.0);
+}
+
+TEST(TestGaussians, calculateGaussianAsymmetrical) {
+	// despite huge standard deviations, the value at mean will be 1
+	auto g = calculateGaussianAsymmetrical(0.0, 0.0, 0.0, 0.0, 0.0, 1e06, 1e06, 1e06);
+	ASSERT_DOUBLE_EQ(g, 1.0);
+}
+
 // Ref: http://blog.sarantop.com/notes/mvn
 TEST(TestGaussians, multivariateMatrixForm) {
 	// Define the covariance matrix, the mean and test state
