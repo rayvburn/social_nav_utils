44126ec99000e9a9a8515838ec278a3b01dd4dc2
Jarek Karwowski <chromedivizer@gmail.com>
2023-02-06 01:50:21 +0100

    extended `gaussians` with `Eigen`-oriented functions, added unit test

--- include/social_nav_utils/gaussians.h
+++ include/social_nav_utils/gaussians.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <eigen3/Eigen/Core>
+
 namespace social_nav_utils {
 
 /**
@@ -35,6 +37,36 @@ double calculateGaussianAsymmetrical(
 	double sigma_h,
 	double sigma_r,
 	double sigma_s
+);
+
+/**
+ * @brief Computes a value of Gaussian described with mean vector and covariance matrix
+ *
+ * This is the most handy for multivariate Gaussians
+ *
+ * Based on http://blog.sarantop.com/notes/mvn
+ */
+double calculateGaussian(const Eigen::VectorXd& x, const Eigen::VectorXd& mean, const Eigen::MatrixXd& cov);
+
+/**
+ * @brief Computes a value of asymmetrical Gaussian described with a mean (at least 2 elem.) and 2 covariance matrices
+ *
+ * Evaluates whether x is located in front of the mean and selects appropriate covariance matrix to compute PDF
+ *
+ * @param x vector for whom the PDF is computed for
+ * @param mean mean of the Gaussian
+ * @param mean_orientation 'orientation' of the mean of the Gaussian (assuming that x does not contain this information)
+ * @param cov_front covariance matrix for the front case
+ * @param cov_rear covariance matrix for the rear case
+ * @param unify_cov_scale adjusts scale of the output to avoid a step when front/rear covariances strongly differ
+ */
+double calculateGaussianAsymmetrical(
+	const Eigen::VectorXd& x,
+	const Eigen::VectorXd& mean,
+	double mean_orientation,
+	const Eigen::MatrixXd& cov_front,
+	const Eigen::MatrixXd& cov_rear,
+	bool unify_cov_scale = false
 );
 
 } // namespace social_nav_utils
