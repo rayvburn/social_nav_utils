61bf7bebb383e2d50f5a2670961c6612011c84d6
Jarek Karwowski <chromedivizer@gmail.com>
2023-04-21 15:52:33 +0200

    added more test cases to Gaussian-based social penalties

--- test/test_formation_space_intrusion.cpp
+++ test/test_formation_space_intrusion.cpp
@@ -33,6 +33,30 @@ TEST(TestMetricGaussian, formationSpaceGaussian) {
 		4.05000000000000 /* robot_pos_y */
 	);
 	EXPECT_NEAR(gaussian2, 0.0254331283458186, 1e-05);
+
+	/*
+	* Matlab:
+	* ellipseGeometricToStandard():
+	*   x0 = 4.0;
+	*   y0 = 2.0;
+	*   aaxis = 1.0;
+	*   baxis = 0.5;
+	*   phi = -pi/6;
+	* cov_fformation_uncert = [0.87654, 0.67892; 0.67892, 1.09876];
+	*/
+	double gaussian3 = FormationSpaceIntrusion::computeFormationSpaceGaussian(
+		4.0000, /* ospace_pos_x */
+		2.0000, /* ospace_pos_y */
+		-0.523598775598299, /* ospace_orientation */
+		0.250000000000000, /* ospace_variance_x */
+		0.062500000000000, /* ospace_variance_y */
+		0.87654, /* pos_center_variance_xx */
+		0.67892, /* pos_center_variance_xyyx */
+		1.09876, /* pos_center_variance_yy */
+		3.45, /* robot_pos_x */
+		1.75  /* robot_pos_y */
+	);
+	EXPECT_NEAR(gaussian3, 0.141921, 1e-05);
 }
 
 int main(int argc, char** argv) {
